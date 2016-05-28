15688cd3eb4b832829172c2aeade5fb0374ede83
Juan Sebastian Cifuentes <juanse83@gmail.com>
2016-05-27 23:42:55 -0500

    fix errors on compile

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,4 @@
+/// <reference path="../typings/browser.d.ts" />
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './component/app.component';
 bootstrap(AppComponent);
